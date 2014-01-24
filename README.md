06943667030e51e576bc3c886dc4a50fa13f5cb3
TerrorJack <astrohavoc@gmail.com>
2014-01-24 21:21:36 +0800

    will take a while before next commit :(
    Signed-off-by: TerrorJack <astrohavoc@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-tsinghua.py
-===========
+# tsinghua.py
 
-Tsinghua University for humans :D
+Make your Tsinghua campus life pythonic!
+
+## What we've got
+
+* thu_net.py : Dealing with campus network.
+
+## What we'll have
+
+* Lots. Most probably info/web learning bot. Certain functionalities will be developed but not published.
+
+## How to use
+
+Read the function name and you'll know how to use :D
+
+Seriously: I'll write more comment to help you with that. I'll also build a GUI (after learning how to).
---
+++ thu_net.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import hashlib
+
+
+def is_online():
+    return ',' in requests.post('http://net.tsinghua.edu.cn/cgi-bin/do_login', data={'action': 'check_online'}).content
+
+
+def login(username, password):
+    return requests.post('http://net.tsinghua.edu.cn/cgi-bin/do_login',
+                         data={'username': username, 'password': hashlib.md5(password).hexdigest(), 'drop': 0,
+                               'type': 1, 'n': 100}).content
+
+
+def logout():
+    return requests.post('http://net.tsinghua.edu.cn/cgi-bin/do_logout').content
